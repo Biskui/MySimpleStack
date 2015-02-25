fc3fddefac9473beda0f9f9d2a3112b5dce3227a
Biskui <aurelie.mercier3131@gmail.com>
2015-02-25 13:57:35 +0100

    2ème commit : ajout des fichiers du tp2

---
+++ src/main/java/dcll/amer/Item.java
@@ -0,0 +1,20 @@
+package dcll.amer;
+
+/**
+ * Created by Yui on 09/02/2015.
+ */
+public class Item {
+    private String item;
+
+    public Item(String item) {
+        this.item = item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+}
